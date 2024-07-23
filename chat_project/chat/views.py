2d25f49ece13a52190c756d83d61f83e995c8fe7
gregorykariara1@gmail.com <gregorykariara1@gmail.com>
2024-07-23 11:58:37 +0300

    Rearranged

---
+++ chat_project/chat/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+def land_page(request):
+    pass
+
+def login_page(request):
+    pass
+
+def sign_up(request):
+    pass
+
+def chat_message_close_up(request):
+    pass

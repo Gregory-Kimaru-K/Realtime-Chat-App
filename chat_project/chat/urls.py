968a257a0cd2680c6b831c28ddd8841b5570dde1
gregorykariara1@gmail.com <gregorykariara1@gmail.com>
2024-07-24 13:40:01 +0300

    Something about urls

--- chat_project/chat/urls.py
+++ chat_project/chat/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.overview_msg, name='land'),
-    path("/grp", views.grp, name='grp'),
-    path("/theme", views.theme, name='theme')
+    path("grp/", views.grp, name='grp'),
+    path("theme/", views.theme, name='theme')
 ]
